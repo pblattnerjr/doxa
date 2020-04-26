e496176b0562d6643beb5a327b49d8f3d6fd4316
Michael Colburn <mac002@theColburns.us>
2020-04-26 16:01:55 -0400

    WIP

--- cmd/doxa/go.mod
+++ cmd/doxa/go.mod
@@ -1,5 +1,3 @@
 module ocmc.org/doxa
 
 go 1.14
-
-require github.com/liturgiko/doxa v0.0.0-20200423195741-31ba96a799e2 // indirect
--- cmd/doxa/main.go
+++ cmd/doxa/main.go
@@ -1,7 +1,7 @@
 package main
 
-import webapp "github.com/liturgiko/doxa/pkg/server/app"
+import "github.com/liturgiko/doxa/pkg/server/app"
 
 func main() {
-	webapp.Serve("$HOME/doxa/data/sql/liturgical.db","8080", "8090")
+	app.Serve("$HOME/doxa/data/sql/liturgical.db","8080", "8090")
 }
